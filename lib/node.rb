ed717da792d3161a4d852826d097460a408ce961
Jeff Nelson <jpnelson85@gmail.com>
2023-03-23 19:13:13 -0500

    add next_node test

--- lib/node.rb
+++ lib/node.rb
@@ -6,3 +6,9 @@ describe Node do
     node = Node.new('plop')
     expect(node).to be_a(Node)
   end
+
+  it 'next_node equals nil as default' do
+    node = Node.new('plop')
+    expect(node.next_node).to eq(nil)
+  end
+end

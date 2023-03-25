1e232527e52277bf1b19239184e8e6475cad1f8b
Jeff Nelson <jpnelson85@gmail.com>
2023-03-25 14:46:54 -0500

    add linked list tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -13,16 +13,43 @@ describe LinkedList do
     expect(list.head).to eq(nil)
   end
 
-  it 'append sounds' do
+  it 'append one sound' do
     list = LinkedList.new
     list.append('doop')
     expect(list.head.data).to eq('doop')
+    expect(list.head.next_node).to eq(nil)
+    expect(list.count).to eq(1)
+    expect(list.to_string).to eq("doop")
   end
 
-  it 'returns correct count of sounds' do
+  it 'appends two sounds' do
     list = LinkedList.new
     list.append('doop')
     list.append('deep')
+    expect(list.head.data).to eq('doop')
+    expect(list.head.next_node).to eq('deep')
     expect(list.count).to eq(2)
+    expect(list.to_string).to eq("doop deep") 
   end
+
+  it 'adds node to beginning as the head' do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+  end
+
+  it 'inserts new data into a specified index' do 
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    list.insert(1,"woo")
+    expect(list.insert(1,"woo")).to eq("woo")
+    expect(list.count).to eq(5)
+    expect(list.head.next_node.data).to eq("woo")
+  end
+
 end
